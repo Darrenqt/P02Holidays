7cb83f76c1bbb288555b72d03f0fb12fdbbc3eb4
16022809 <darren410ss@gmail.com>
2018-04-23 22:06:15 +0800

    holidays java page

--- app/src/main/java/com/example/a16022809/p02_holidays/Holidays.java
+++ app/src/main/java/com/example/a16022809/p02_holidays/Holidays.java
@@ -1,4 +1,14 @@
 package com.example.a16022809.p02_holidays;
 
 public class Holidays {
+    private String holiday;
+    private String date;
+    public Holidays(String holiday, String date) {
+        this.holiday = holiday;
+        this.date = date;
+    }
+    public String getHoliday() {
+        return holiday;
+    }
+    public String getDate() { return date; }
 }
